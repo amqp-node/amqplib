dede003f87c7dd7cb339a798471eb03ced5b3d83
Michael Bridgen <mikeb@squaremobius.net>
2014-04-22 15:10:27 +0100

    Some "polymorphism" for the callback API entry point

--- callback_api.js
+++ callback_api.js
@@ -1,8 +1,17 @@
 var raw_connect = require('./lib/connect').connect;
 var CallbackModel = require('./lib/callback_model').CallbackModel;
 
-function connect(url, connOptions, cb) {
-  raw_connect(url, connOptions).then(function(c) {
+// Supports three shapes:
+// connect(url, options, callback)
+// connect(url, callback)
+// connect(callback)
+function connect(url, options, cb) {
+  if (typeof url === 'function')
+    cb = url, url = false, options = false;
+  else if (typeof options === 'function')
+    cb = options, options = false;
+
+  raw_connect(url, options).then(function(c) {
     cb(null, new CallbackModel(c));
   }, cb);
 };
