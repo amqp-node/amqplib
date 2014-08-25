6090dea77fe6968b22a25a02295e2dd32a436786
Michael Bridgen <mikeb@squaremobius.net>
2014-08-25 17:08:34 +0100

    Add waitForConfirms to promise API
    An experimental waitForConfirms. This one works by replacing each
    callback in the unconfirmed messages window with a promise-resolver,
    and returning a promise that resolves when all of those do.

---
+++ examples/waitForConfirms.js
@@ -0,0 +1,22 @@
+var amqp = require('../');
+
+var NUM_MSGS = 20;
+
+function mkCallback(i) {
+  return (i % 2) === 0 ? function(err) {
+    if (err !== null) { console.error('Message %d failed!', i); }
+    else { console.log('Message %d confirmed', i); }
+  } : null;
+}
+
+amqp.connect().then(function(c) {
+  c.createConfirmChannel().then(function(ch) {
+    for (var i=0; i < NUM_MSGS; i++) {
+      ch.publish('amq.topic', 'whatever', new Buffer('blah'), {}, mkCallback(i)); 
+    }
+    ch.waitForConfirms().then(function() {
+      console.log('All messages done');
+      c.close();
+    }, console.error);
+  });
+});
