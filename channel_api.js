8f1795812da047ee76ce739882a80af5cc0a8e99
Kris Reeves <kris.re@bbhmedia.com>
2015-05-26 13:48:39 -0400

    Missed a file

--- channel_api.js
+++ channel_api.js
@@ -1,14 +1,14 @@
 var raw_connect = require('./lib/connect').connect;
 var ChannelModel = require('./lib/channel_model').ChannelModel;
-var defer = require('when').defer;
+var Promise = require('bluebird');
 
 function connect(url, connOptions) {
-  var opened = defer();
-  raw_connect(url, connOptions, function(err, conn) {
-    if (err === null) opened.resolve(new ChannelModel(conn));
-    else opened.reject(err);
+  return new Promise(function (resolve, reject) {
+    raw_connect(url, connOptions, function(err, conn) {
+      if (err === null) resolve(new ChannelModel(conn));
+      else reject(err);
+    });
   });
-  return opened.promise;
 };
 
 module.exports.connect = connect;
