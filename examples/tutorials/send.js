828928531325b59b514b355b3e0ab9c3d118eb07
Jeff Lee <leejeffc@gmail.com>
2015-06-23 11:05:01 -0700

    Refactor example tutorial (send)

--- examples/tutorials/send.js
+++ examples/tutorials/send.js
@@ -1,19 +1,41 @@
 #!/usr/bin/env node
 
 var amqp = require('amqplib');
-var when = require('when');
 
-amqp.connect('amqp://localhost').then(function(conn) {
-  return when(conn.createChannel().then(function(ch) {
-    var q = 'hello';
-    var msg = 'Hello World!';
+function send(url, queueName, message) {
+  var connection;
 
-    var ok = ch.assertQueue(q, {durable: false});
-    
-    return ok.then(function(_qok) {
-      ch.sendToQueue(q, new Buffer(msg));
-      console.log(" [x] Sent '%s'", msg);
-      return ch.close();
-    });
-  })).ensure(function() { conn.close(); });;
-}).then(null, console.warn);
+  function createChannel(_connection) {
+    connection = _connection;
+    return connection.createChannel();
+  }
+
+  function setupQueue(channel) {
+    var queueConfig = {durable: false};
+    return channel.assertQueue(queueName, queueConfig);
+  }
+
+  function sendMsgToQueue(channel) {
+    channel.sendToQueue(queueName, new Buffer(message));
+  }
+  
+  function closeChannel(channel) {
+    return channel.close();
+  }
+
+  function closeConnection() {
+    if(connection) {
+      connection.close();
+    }
+  }
+  
+  return amqp.connect(url)
+    .then(createChannel)
+    .tap(setupQueue)
+    .tap(sendMsgToQueue)
+    .then(closeChannel)
+    .catch(console.warn)
+    .finally(closeConnection);
+}
+
+send('amqp://localhost', 'hello', 'Hello, World!');
