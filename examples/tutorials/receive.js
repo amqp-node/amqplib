1f6cff780679c5b84a847a5f55b3f59611f570b4
lopesleandrorj <53540938+lopesleandrorj@users.noreply.github.com>
2020-07-31 20:21:20 -0300

    ES6 updates
    Applying ES6 to tutorial code: Arrow functions and replacing "var" by "let"

--- examples/tutorials/receive.js
+++ examples/tutorials/receive.js
@@ -2,20 +2,22 @@
 
 var amqp = require('amqplib');
 
-amqp.connect('amqp://localhost').then(function(conn) {
-  process.once('SIGINT', function() { conn.close(); });
-  return conn.createChannel().then(function(ch) {
+amqp.connect('amqp://localhost').then(conn => {
+  process.once('SIGINT', () => { 
+    conn.close() 
+  })
+  
+  return conn.createChannel()
+    .then(ch => {
+      let ok = ch.assertQueue('hello', { durable: false })
+      ok = ok.then(_qok => {
+        return ch.consume('hello', msg => {
+          console.log("[x] Received '%s'", msg.content.toString())
+        }, { noAck: true })
+      })
 
-    var ok = ch.assertQueue('hello', {durable: false});
-
-    ok = ok.then(function(_qok) {
-      return ch.consume('hello', function(msg) {
-        console.log(" [x] Received '%s'", msg.content.toString());
-      }, {noAck: true});
-    });
-
-    return ok.then(function(_consumeOk) {
-      console.log(' [*] Waiting for messages. To exit press CTRL+C');
-    });
-  });
-}).catch(console.warn);
+      return ok.then(_consumeOk => {
+        console.log('[*] Waiting for messages. To exit press CTRL+C')
+      })
+  })
+}).catch(console.warn)
