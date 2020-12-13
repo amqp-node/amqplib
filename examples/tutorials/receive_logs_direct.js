5d6c6ab2fc0920e25a7a952efbbd2d9cac0b467e
Jeremy Clarke <jeremy@savetrees.co.uk>
2020-12-13 19:58:54 +0000

    Update Promise usage

--- examples/tutorials/receive_logs_direct.js
+++ examples/tutorials/receive_logs_direct.js
@@ -1,45 +1,51 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib');
-var all = require('bluebird').all;
-var basename = require('path').basename;
+var amqp = require("amqplib");
+var basename = require("path").basename;
 
 var severities = process.argv.slice(2);
 if (severities.length < 1) {
-  console.warn('Usage: %s [info] [warning] [error]',
-               basename(process.argv[1]));
+  console.warn("Usage: %s [info] [warning] [error]", basename(process.argv[1]));
   process.exit(1);
 }
 
-amqp.connect('amqp://localhost').then(function(conn) {
-  process.once('SIGINT', function() { conn.close(); });
-  return conn.createChannel().then(function(ch) {
-    var ex = 'direct_logs';
-
-    var ok = ch.assertExchange(ex, 'direct', {durable: false});
-
-    ok = ok.then(function() {
-      return ch.assertQueue('', {exclusive: true});
+amqp
+  .connect("amqp://localhost")
+  .then(function (conn) {
+    process.once("SIGINT", function () {
+      conn.close();
     });
 
-    ok = ok.then(function(qok) {
-      var queue = qok.queue;
-      return all(severities.map(function(sev) {
-        ch.bindQueue(queue, ex, sev);
-      })).then(function() { return queue; });
-    });
+    return conn.createChannel().then(function (ch) {
+      var ex = "direct_logs";
 
-    ok = ok.then(function(queue) {
-      return ch.consume(queue, logMessage, {noAck: true});
-    });
-    return ok.then(function() {
-      console.log(' [*] Waiting for logs. To exit press CTRL+C.');
-    });
+      var ok = ch.assertExchange(ex, "direct", { durable: false });
 
-    function logMessage(msg) {
-      console.log(" [x] %s:'%s'",
-                  msg.fields.routingKey,
-                  msg.content.toString());
-    }
-  });
-}).catch(console.warn);
+      ok = ok.then(function () {
+        return ch.assertQueue("", { exclusive: true });
+      });
+
+      ok = ok.then(function (qok) {
+        var queue = qok.queue;
+        return Promise.all(
+          severities.map(function (sev) {
+            ch.bindQueue(queue, ex, sev);
+          }),
+        ).then(function () {
+          return queue;
+        });
+      });
+
+      ok = ok.then(function (queue) {
+        return ch.consume(queue, logMessage, { noAck: true });
+      });
+      return ok.then(function () {
+        console.log(" [*] Waiting for logs. To exit press CTRL+C.");
+      });
+
+      function logMessage(msg) {
+        console.log(" [x] %s:'%s'", msg.fields.routingKey, msg.content.toString());
+      }
+    });
+  })
+  .catch(console.warn);
