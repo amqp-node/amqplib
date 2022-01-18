6aaa99fea05bacffc2dfaff551c0e01df878c98a
Jeremy Clarke <jeremy@savetrees.co.uk>
2022-01-18 12:20:33 +0000

    Minor tweaks pre-publish

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,15 @@
 module.exports = {
   env: {
     es6: true,
-    node: true
+    node: true,
   },
-  extends: ['eslint:recommended', 'plugin:import/errors', 'plugin:import/warnings', 'prettier'],
+  extends: ["eslint:recommended", "plugin:import/errors", "plugin:import/warnings", "prettier"],
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: "module",
   },
   rules: {
-    //'no-unused-vars': ['error', { args: 'none' }],
-    //'no-console': 'off',
-    //'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    //'no-duplicate-imports': 'error',
-    //'require-atomic-updates': 'off',
-    //indent: ['error', 2],
-    //'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'double', { avoidEscape: true }],
-    semi: ['error', 'always']
-    //'brace-style': ['error', '1tbs', { allowSingleLine: true }]
-  }
+    quotes: ["error", "double", { avoidEscape: true }],
+    semi: ["error", "always"],
+  },
 };
