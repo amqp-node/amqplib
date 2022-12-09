288d9199da80a8df7137da9d0b19399c8038a913
Steve Cresswell <229672+cressie176@users.noreply.github.com>
2022-12-09 19:17:04 +0000

    Update examples

--- examples/tutorials/emit_log_topic.js
+++ examples/tutorials/emit_log_topic.js
@@ -1,19 +1,26 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib');
+const amqp = require('amqplib');
 
-var args = process.argv.slice(2);
-var key = (args.length > 0) ? args[0] : 'info';
-var message = args.slice(1).join(' ') || 'Hello World!';
+const exchange = 'topic_logs';
+const args = process.argv.slice(2);
+const routingKeys = (args.length > 0) ? args[0] : 'info';
+const text = args.slice(1).join(' ') || 'Hello World!';
 
-amqp.connect('amqp://localhost').then(function(conn) {
-  return conn.createChannel().then(function(ch) {
-    var ex = 'topic_logs';
-    var ok = ch.assertExchange(ex, 'topic', {durable: false});
-    return ok.then(function() {
-      ch.publish(ex, key, Buffer.from(message));
-      console.log(" [x] Sent %s:'%s'", key, message);
-      return ch.close();
-    });
-  }).finally(function() { conn.close(); })
-}).catch(console.log);
+(async () => {
+  let connection;
+  try {
+    connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+    await channel.assertExchange(exchange, 'topic', { durable: false });
+    channel.publish(exchange, routingKeys, Buffer.from(text));
+    console.log(" [x] Sent %s:'%s'", routingKeys, text);
+    await channel.close();
+  }
+  catch (err) {
+    console.warn(err);
+  }
+  finally {
+    if (connection) await connection.close();
+  };
+})();  
