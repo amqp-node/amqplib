c5699ff4f659b1680de2f68b78954bceeeda47b5
Steve Cresswell <229672+cressie176@users.noreply.github.com>
2022-12-09 19:17:04 +0000

    Update examples

--- examples/tutorials/receive.js
+++ examples/tutorials/receive.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib');
+const amqp = require('amqplib');
 
-amqp.connect('amqp://localhost').then(function(conn) {
-  process.once('SIGINT', function() { conn.close(); });
-  return conn.createChannel().then(function(ch) {
-
-    var ok = ch.assertQueue('hello', {durable: false});
-
-    ok = ok.then(function(_qok) {
-      return ch.consume('hello', function(msg) {
-        console.log(" [x] Received '%s'", msg.content.toString());
-      }, {noAck: true});
+(async () => {
+  try {
+    const connection = await amqp.connect('amqp://localhost');
+    process.once('SIGINT', async () => { 
+      await connection.close();
     });
+    const channel = await connection.createChannel();
+    await channel.assertQueue('hello', { durable: false });
+    await channel.consume('hello', (message) => {
+      console.log(" [x] Received '%s'", message.content.toString());
+    }, { noAck: true });
 
-    return ok.then(function(_consumeOk) {
-      console.log(' [*] Waiting for messages. To exit press CTRL+C');
-    });
-  });
-}).catch(console.warn);
+    console.log(' [*] Waiting for messages. To exit press CTRL+C');
+  } catch (err) {
+    console.warn(err);
+  }
+})();
