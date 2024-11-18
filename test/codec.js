16a7d3372d5d3dc1d2b4c5866002f670120aece6
Stephen Cresswell <229672+cressie176@users.noreply.github.com>
2024-11-18 23:26:50 +0000

    Complete support for unsigned integers

--- test/codec.js
+++ test/codec.js
@@ -44,19 +44,16 @@ var testCases = [
     ['null', {'void': null}, [4,118,111,105,100,86]],
 
     // array, object
-    ['array', {array: [6, true, "foo"]},
-     [5,97,114,114,97,121,65,0,0,0,12,98,6,116,1,83,0,0,0,3,102,111,111]],
-    ['object', {object: {foo: "bar", baz: 12}},
-     [6,111,98,106,101,99,116,70,0,0,0,18,3,102,111,111,83,0,
-      0,0,3,98,97,114,3,98,97,122,98,12]],
+    ['array', {array: [6, true, "foo"]},[5,97,114,114,97,121,65,0,0,0,12,98,6,116,1,83,0,0,0,3,102,111,111]],
+    ['object', {object: {foo: "bar", baz: 12}},[6,111,98,106,101,99,116,70,0,0,0,18,3,102,111,111,83,0,0,0,3,98,97,114,3,98,97,122,98,12]],
 
     // exotic types
-    ['timestamp', {timestamp: {'!': 'timestamp', value: 1357212277527}},
-     [9,116,105,109,101,115,116,97,109,112,84,0,0,1,60,0,39,219,23]],
-    ['decimal', {decimal: {'!': 'decimal', value: {digits: 2345, places: 2}}},
-     [7,100,101,99,105,109,97,108,68,2,0,0,9,41]],
-    ['float', {float: {'!': 'float', value: 0.1}},
-     [5,102,108,111,97,116,102,61,204,204,205]],
+    ['timestamp', {timestamp: {'!': 'timestamp', value: 1357212277527}},[9,116,105,109,101,115,116,97,109,112,84,0,0,1,60,0,39,219,23]],
+    ['decimal', {decimal: {'!': 'decimal', value: {digits: 2345, places: 2}}},[7,100,101,99,105,109,97,108,68,2,0,0,9,41]],
+    ['float', {float: {'!': 'float', value: 0.1}},[5,102,108,111,97,116,102,61,204,204,205]],
+    ['unsignedbyte', {unsignedbyte:{'!': 'unsignedbyte', value: 255}}, [12,117,110,115,105,103,110,101,100,98,121,116,101,66,255]],
+    ['unsignedshort', {unsignedshort:{'!': 'unsignedshort', value: 65535}}, [13,117,110,115,105,103,110,101,100,115,104,111,114,116,117,255,255]],
+    ['unsignedint', {unsignedint:{'!': 'unsignedint', value: 4294967295}}, [11,117,110,115,105,103,110,101,100,105,110,116,105,255,255,255,255]],
 ];
 
 function bufferToArray(b) {
@@ -109,6 +106,9 @@ suite("Roundtrip values", function() {
     amqp.Bit,
     amqp.Decimal,
     amqp.Timestamp,
+    amqp.UnsignedByte,
+    amqp.UnsignedShort,
+    amqp.UnsignedInt,
     amqp.Double,
     amqp.Float,
     amqp.FieldArray,
